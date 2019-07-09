1a44ac92f25b5740e19864c49bb88f16714b3e2e
Diego Dorta <diego.dorta@nxp.com>
2019-07-09 18:12:33 -0300

    Add rules handwritten
    Signed-off-by: Diego Dorta <diego.dorta@nxp.com>

--- robin/samples/2-hwd/hwd-cl/handwritten-digit.cpp
+++ robin/samples/2-hwd/hwd-cl/handwritten-digit.cpp
@@ -14,8 +14,8 @@
  * You should have received a copy of the GNU General Public License along with
  * this program. If not, see <https://www.gnu.org/licenses/>.
  */
-#include "robin-nn.h"
-#include "hwd.h"
+#include "robin/robin-nn.h"
+#include "robin/hwd.h"
                          
 int main(int argc, char *argv[])
 {
