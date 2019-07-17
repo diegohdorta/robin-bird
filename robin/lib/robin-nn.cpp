c0bdfd2779cfbfa1e3c6b4c09f6a2f59aacf7f76
Diego Dorta <diego.dorta@nxp.com>
2019-07-17 15:50:56 -0300

    Add time to model file name
    Signed-off-by: Diego Dorta <diego.dorta@nxp.com>

--- robin/lib/robin-nn.cpp
+++ robin/lib/robin-nn.cpp
@@ -179,7 +179,15 @@ void RobinBird::writetoFile(Matrix<double> &m, std::ofstream &file)
 
 void RobinBird::saveRobinBirdModel(const char *fileModelPath)
 {
-    std::ofstream out(fileModelPath);
+    time_t rawtime;
+    struct tm *timeinfo;
+    char buffer[80];
+
+    time(&rawtime);
+    timeinfo = localtime(&rawtime);
+
+    strftime(buffer, 80, "%Y_%m_%d_%H_%M_%S_hwd-model.rb", timeinfo);
+    std::ofstream out(buffer);
     
     out << hiddenLayersCount << std::endl;
     out << learningRate << std::endl;
